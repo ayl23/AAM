a74deb6e41ab6d82133d35e0406e41fad1872daa
ayl23 <34237279+ayl23@users.noreply.github.com>
2018-11-27 19:20:43 +0000

    Changed short name constraint

--- final/contactform/contactform.js
+++ final/contactform/contactform.js
@@ -30,7 +30,7 @@ jQuery(document).ready(function($) {
             break;
 
           case 'minlen':
-            if (i.val().length < parseInt(exp)) {
+            if (i.val().length < parseInt(exp)-1) {
               ferror = ierror = true;
             }
             break;
